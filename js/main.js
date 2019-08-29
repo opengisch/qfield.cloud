9608d5d92c6a18bec5102d984f70ea68966eaabd
Marco Bernasocchi <marco@opengis.ch>
2019-08-29 16:50:07 +0300

    use our mailchimp
    Signed-off-by: Marco Bernasocchi <marco@opengis.ch>

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(function () {
 
 	$('#mc-form').ajaxChimp({
 		callback: callbackFunction,
-		url: 'http://csmthemes.us3.list-manage.com/subscribe/post?u=9666c25a337f497687875a388&id=45b2c44b98'
+		url: 'https://opengis.us3.list-manage.com/subscribe/post?u=2d414f4d56360dcbf83927721&amp;id=dfa55b9546'
 			// http://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx
 	});
 
