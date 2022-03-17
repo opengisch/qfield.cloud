7632b934f8ab870c655291281b2c7d1fa18d1a7d
olivierdalang <olivier.dalang@gmail.com>
2022-03-17 16:36:57 +0100

    reenable smooth scroll

--- js/main.js
+++ js/main.js
@@ -36,4 +36,39 @@ $(function () {
 		});
 	});
 
+
+
+	/* ==========================================================================
+       Smooth scroll
+     ========================================================================== */
+
+
+	$('a[href*="#"]')
+
+	.not('[href="#"]')
+		.not('[href="#0"]')
+		.on('click', function (event) {
+
+			if (
+				location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') &&
+				location.hostname === this.hostname
+			) {
+
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+				if (target.length) {
+
+					event.preventDefault();
+					$('html, body').animate({
+						scrollTop: (target.offset().top - 80)
+					}, 1000);
+					return false;
+				}
+			}
+
+		});
+
+
+
 });
