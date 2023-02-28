df1904c9830d1b124b4da5825a76fac9ec0dc80e
Marco Bernasocchi <marco@opengis.ch>
2023-02-28 11:06:00 +0100

    split JS

--- js/main.js
+++ js/main.js
@@ -69,27 +69,4 @@ $(function () {
       }
     });
 
-  /* ==========================================================================
-       yearly payments
-     ========================================================================== */
-
-  const tableWrapper = document.querySelector("#pricing");
-  const switchInputs = document.querySelectorAll(".switch-wrapper input");
-  const prices = tableWrapper.querySelectorAll("#pricing .price");
-  const toggleClass = "hide";
-
-  for (const switchInput of switchInputs) {
-    switchInput.addEventListener("input", function () {
-      for (const price of prices) {
-        price.classList.add(toggleClass);
-      }
-      const activePrices = tableWrapper.querySelectorAll(
-        `.price.${switchInput.id}`
-      );
-      for (const activePrice of activePrices) {
-        activePrice.classList.remove(toggleClass);
-      }
-    });
-  }
-
 });
